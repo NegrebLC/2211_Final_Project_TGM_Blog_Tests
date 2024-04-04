9f16b0402d369db8d0e51a1bc3a9d2acf9a9de34
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-04 13:55:44 -0600

    initiated

--- obj/Debug/net8.0/2211_Final_Project_TGM_Blog_Tests.AssemblyInfo.cs
+++ obj/Debug/net8.0/2211_Final_Project_TGM_Blog_Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b57c3d0fb721ad1b7800b57a27f2b820498b2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb33a9e9aef924d3bc44ebcdb795e5de8dbc876")]
 [assembly: System.Reflection.AssemblyProductAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
