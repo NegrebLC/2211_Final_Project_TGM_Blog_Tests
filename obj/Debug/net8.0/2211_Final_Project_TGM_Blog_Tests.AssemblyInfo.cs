67216b18e03d961c70b95757a903478cbaed8d59
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-05 01:38:45 -0600

    added bin and obj to git ignore

--- obj/Debug/net8.0/2211_Final_Project_TGM_Blog_Tests.AssemblyInfo.cs
+++ obj/Debug/net8.0/2211_Final_Project_TGM_Blog_Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb33a9e9aef924d3bc44ebcdb795e5de8dbc876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75009693c0e3314ca834cfb63c38c358bd5ce050")]
 [assembly: System.Reflection.AssemblyProductAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2211_Final_Project_TGM_Blog_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
